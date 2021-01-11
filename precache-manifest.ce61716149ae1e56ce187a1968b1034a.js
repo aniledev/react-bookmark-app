6c9e91f50d7881a7224543a2d1d1b601ff1eb138
aniledev <elinamcgill@gmail.com>
2021-01-10 20:50:09 -0500

    Updates

---
+++ precache-manifest.ce61716149ae1e56ce187a1968b1034a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "b695385ca707626c9269",
+    "url": "/react-bookmark-app/static/css/main.0472c8d4.chunk.css"
+  },
+  {
+    "revision": "b695385ca707626c9269",
+    "url": "/react-bookmark-app/static/js/main.b695385c.chunk.js"
+  },
+  {
+    "revision": "9b7bb550f1bae56a115e",
+    "url": "/react-bookmark-app/static/js/1.9b7bb550.chunk.js"
+  },
+  {
+    "revision": "ff94a5c427eaf77a6585",
+    "url": "/react-bookmark-app/static/js/runtime~main.ff94a5c4.js"
+  },
+  {
+    "revision": "39b9d10638ff374206b858ecaf50e9fd",
+    "url": "/react-bookmark-app/index.html"
+  }
+];
