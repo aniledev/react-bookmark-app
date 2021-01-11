2ced377b9ca2efff097d9971d236e726462a2ed7
aniledev <elinamcgill@gmail.com>
2021-01-10 20:26:09 -0500

    FEAT: validate the URL propType

--- src/BookmarkItem/BookmarkItem.js
+++ src/BookmarkItem/BookmarkItem.js
@@ -29,7 +29,32 @@ export default function BookmarkItem(props) {
 
 BookmarkItem.propTypes = {
   title: PropTypes.string.isRequired,
-  url: PropTypes.string.isRequired,
+  url: (props, propName, componentName) => {
+    // get the value of the prop
+    const prop = props[propName];
+
+    // do the isRequired check
+    if (!prop) {
+      return new Error(
+        `${propName} is required in ${componentName}. Validation Failed`
+      );
+    }
+
+    // check the type
+    if (typeof prop != "string") {
+      return new Error(
+        `Invalid prop, ${propName} is expected to be a string in ${componentName}. ${typeof prop} found.`
+      );
+    }
+
+    // do the custom check here
+    // using a simple regex
+    if (prop.length < 5 || !prop.match(new RegExp(/^https?:\/\//))) {
+      return new Error(
+        `Invalid prop, ${propName} must be min length 5 and begin http(s)://. Validation Failed.`
+      );
+    }
+  },
   rating: PropTypes.number,
   description: PropTypes.string,
 };
