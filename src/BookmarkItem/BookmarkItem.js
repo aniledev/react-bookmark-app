400657c5b56b95e901589827218b57447a7b4c96
aniledev <elinamcgill@gmail.com>
2021-01-16 22:38:01 -0500

    FEAT: build state version of BookmarkItem component

---
+++ src/BookmarkItem/BookmarkItem.js
@@ -0,0 +1,25 @@
+// import react component
+import React, { Component } from "react";
+
+// import css file
+import "./BookmarkItem";
+// import children components
+import Rating from "../Rating/Rating";
+
+// create function component with children component
+
+export default function BookmarkItem(props) {
+  return (
+    <div className="BookmarkItem">
+      <div className="BookmarkItem__row">
+        <div className="BookmarkItem__title">
+          <a href={props.url} target="_blank" rel="noopener noreferrer">
+            {props.title}
+          </a>
+        </div>
+        <Rating value={props.rating} />
+      </div>
+      <div className="BookmarkItem__description">{props.description}</div>
+    </div>
+  );
+}
