0cabdbd4583daf34b65f7d3f540901c7d0f069b7
aniledev <elinamcgill@gmail.com>
2021-01-16 22:20:47 -0500

    FEAT: build static Fab component

---
+++ src/Fab/Fab.js
@@ -0,0 +1,9 @@
+// import react component
+import React from "react";
+// import css files
+import "./Fab.css";
+
+// create function component
+export default function Fab() {
+  return <div className="Fab">&#43;</div>;
+}
