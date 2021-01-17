c6d83fd901d6e70d23523ff4f10db29e0caeff29
aniledev <elinamcgill@gmail.com>
2021-01-16 21:53:51 -0500

    FEAT: build static version of BookmarkApp component

---
+++ src/BookmarkApp/BookmarkApp.js
@@ -0,0 +1,22 @@
+// import react component
+import React, { Component } from "react";
+// import css file
+import "./BookmarkApp.css";
+// bookmark list component
+import BookmarkList from "../BookmarkList/BookmarkList";
+// import fab component
+import Fab from "../Fab/Fab";
+
+// create class component with children component bookmarklist and fab
+
+export default class BookmarkApp extends Component {
+  render() {
+    return (
+      <div className="BookmarkApp">
+        <h2>Bookmarks</h2>
+        <BookmarkList bookmarks={this.props.bookmarks} />
+        <Fab />
+      </div>
+    );
+  }
+}
