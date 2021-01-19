1831db9d2c23af01ee646d457c0d6238d4e2f37e
aniledev <elinamcgill@gmail.com>
2021-01-19 00:01:17 -0500

    CHORE: delete unused variables

--- src/App.js
+++ src/App.js
@@ -7,22 +7,6 @@ import BookmarkApp from "./BookmarkApp/BookmarkApp";
 // import AddBookmark component
 import AddBookmark from "./AddBookmark/AddBookmark";
 
-// create sample test data
-const bookmarks = [
-  {
-    title: "Thinkful",
-    url: "http://www.thinkful.com",
-    rating: "5",
-    description: "A place to receive great programming instruction"
-  },
-  {
-    title: "Google",
-    url: "http://www.google.com",
-    rating: "3",
-    description: "Where to go for all your coding problems"
-  }
-];
-
 // construct class component with two children components
 
 export default class App extends Component {
